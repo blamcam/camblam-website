cfbcc653e4d9ea68327f853bc58fc5038711a9e1
Cameron Murray <cameron-murray@live.com>
2024-04-11 02:32:57 +1000

    Docs Redesign

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
 $(function(){
   $('.notes-container').masonry({
-  // options
-  itemSelector: '.grid-item',
-  columnWidth: '.grid-item',
-  percentPosition: true
-});
+    // options
+    itemSelector: '.grid-item',
+    columnWidth: '.grid-item',
+    percentPosition: true
+  });
+  
+  hljs.highlightAll();
+
+  $(document).on('click', 'a[href^="#"]', function (event) {
+      event.preventDefault();
+
+      if ($.attr(this, 'href') == "#top") {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 500);
+      }
+      else {
+        $('html, body').animate({
+            scrollTop: ($($.attr(this, 'href')).offset().top - 50)
+        }, 500);
+      }
+  });
+
+  $(".wiggle").each(function(index) {
+    var characters = $(this).text().split("");
+    
+    $this = $(this);
+    $this.empty();
+    $.each(characters, function (i, el) {
+      $this.append("<span style='--n:" + (index + i) + "'>" + el + "</span");
+    });
+  });
+
 });
